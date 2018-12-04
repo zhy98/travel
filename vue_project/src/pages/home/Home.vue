437ea2608c38a1385b8bc3b107e1ac750b8f18e6
zhy <2296410585@qq.com>
2018-12-04 18:52:32 +0800

    statrt

---
+++ vue_project/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<home-header></home-header>
+	</div>
+
+</template>
+<script>
+//引入
+import HomeHeader from './components/Header'
+//抛出
+export default {
+		name: 'Home' ,
+		components:{
+			HomeHeader
+		}
+}
+</script>
+<style><style>
---
+++ vue_project/src/pages/home/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="header">
+		<div class="header-left"><div class="iconfont back-icon">&#xe65d;</div></div>
+		<div class="header-input"><span class="iconfont">&#xe61e;</span>输入景点/酒店/游玩主题</div>
+		<div class="header-right">城市<span class="iconfont arrow-icon">&#xe65c;</span></div>
+	</div>
+
+</template>
+<script>
+export default {
+		name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+
+	.header
+		display: flex 
+		line-height:.86rem
+		background:#00bcd4
+		color:#fff
+		.header-left
+			float:left
+			width:.86rem
+			.back-icon
+				text-align:center
+				font-size:.4rem
+		.header-right
+			text-align:center
+			float:right
+			width:1.23rem
+		.header-input
+			flex:1
+			height:.64rem
+			line-height: .64rem
+			padding-left:.2rem
+			margin-top:.12rem
+			margin-left:.2rem
+			background:#fff
+			border-radius:.1rem
+			color:#ccc
+			.arrow-icon
+				margin-left:-.04rem
+				font-size:.24rem
+</style>
